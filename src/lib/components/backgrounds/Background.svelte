675063a147f7453df3974c2b5fc0db095e1fbbd8
Yousef Suleiman <yousefsuleiman10@gmail.com>
2024-08-28 23:09:00 -0500

    sync

--- src/lib/components/backgrounds/Background.svelte
+++ src/lib/components/backgrounds/Background.svelte
@@ -7,7 +7,7 @@
 
 <div data-theme={theme ? "dark" : "light"} class="bg-transparent">
     <div 
-        class="bg-cover bg-top bg-no-repeat bg-fixed fixed w-screen h-screen -z-50 bg-base-100"
+        class="bg-cover bg-top bg-no-repeat fixed w-screen h-screen -z-50 bg-base-100"
         style="background-image: url({theme ? darkSrc : lightSrc}); image-rendering: pixelated;"
     />
     <slot/>
